6a343f8788147559f37561739316868c858f7759
Rozh <rozhax999@gmail.com>
2026-09-03 15:17:39 +0300

    deleted

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SalesModule } from './sales/sales.module';
-import { InventoryModule } from './inventory/inventory.module';
-
-@Module({
-  imports: [SalesModule, InventoryModule],
-})
-export class AppModule {}
